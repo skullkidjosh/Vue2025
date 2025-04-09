e639e9aa1bba9ff706195bda70f94bf333be2bc6
AbrahamAguilar <abrahamaguilar93@outlook.com>
2025-04-08 20:48:39 -0600

    Proyecto modificado para eliminar archivos generados por Vue.

---
+++ PrimerProyectoVue/src/components/Contador.vue
@@ -0,0 +1,36 @@
+<template>
+<h1>Ejemplo de Reactividad</h1>
+<p>{{ contador }}</p>
+<button @click="Incrementar">Incrementar cuenta</button>
+<button @click="Decrementar">Decrementar cuenta</button>
+<button @click="Eliminar">Eliminar cuenta</button>
+  </template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(1);
+const Incrementar = () => {
+  contador.value++;
+}
+
+const Decrementar = () => {
+  contador.value--;
+}
+
+const Eliminar = () => {
+  contador.value=0;
+}
+
+</script>
+
+<style scoped>
+p {
+  color: rgb(240, 19, 19);
+  text-align: center;
+}
+
+h1 {
+  color: rgb(150, 207, 133);
+  text-align: center;
+}
+</style>
